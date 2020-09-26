5864eeb8e967bc8b03f9d4b6e66a2bb009ef88fb
PhilippNox <philipp.nox@gmail.com>
2020-09-26 19:40:23 +0300

    Added url checker

--- app/config.py
+++ app/config.py
@@ -3,6 +3,7 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
 	DEBUG:			bool
+	DEFAULT_SCHEMA: str
 
 	class Config:
 		env_file = '.env'
--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi.logger import logger
 
 import app.schemas as schm
 from app.config import settings
+from app.url_checker import UrlChecker
 
 from typing import Optional
 import logging
@@ -24,6 +25,11 @@ async def short_link(url: str, response: Response, cookie: Optional[str] = Cooki
 		if cookie is None:
 			cookie = str(uuid.uuid4())
 			response.set_cookie(key="cookie", value=cookie)
+
+		# step 1 - check url
+		url_is_good, url, check_msg = UrlChecker.check(url)
+		if url_is_good is False:
+			return schm.Report(ok=False, msg=check_msg, original_url=url, cookie=cookie)
 
 		return schm.Report(ok=True, msg='ok', original_url=url, cookie=cookie)
 
