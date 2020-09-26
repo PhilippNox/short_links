7f76da1dfdd961ed1a3a280d18c8bc888cf6b5c5
PhilippNox <philipp.nox@gmail.com>
2020-09-26 20:21:11 +0300

    Update migration for redirects - utcnow

--- alembic/versions/01_create_redirects_f766bae1853c.py
+++ alembic/versions/01_create_redirects_f766bae1853c.py
@@ -9,6 +9,21 @@ from alembic import op
 import sqlalchemy as sa
 from sqlalchemy.dialects.postgresql import UUID, JSONB
 
+# For utcnow
+# https://docs.sqlalchemy.org/en/13/core/compiler.html#utc-timestamp-function
+from sqlalchemy.sql import expression
+from sqlalchemy.ext.compiler import compiles
+from sqlalchemy.types import DateTime
+
+
+class utcnow(expression.FunctionElement):
+    type = DateTime()
+
+
+@compiles(utcnow, 'postgresql')
+def pg_utcnow(element, compiler, **kw):
+    return "TIMEZONE('utc', CURRENT_TIMESTAMP)"
+
 
 # revision identifiers, used by Alembic.
 revision = 'f766bae1853c'
@@ -21,7 +36,7 @@ def upgrade():
     op.create_table(
         'redirects',
         sa.Column('id', sa.Integer, primary_key=True),
-        sa.Column('created_at', sa.DateTime, server_default=sa.sql.func.now()),
+        sa.Column('created_at', sa.DateTime, server_default=utcnow()),
         sa.Column('cookie', UUID),
         sa.Column('code', sa.String(32), nullable=False, unique=True, index=True),
         sa.Column('link', sa.String(2083), nullable=False),
